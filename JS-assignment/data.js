b3501d080acd8c388f93a6117fd24aee511f60cc
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-19 11:30:46 +0530

    Add files via upload

---
+++ JS-assignment/data.js
@@ -0,0 +1,36 @@
+let songs =[
+    {
+name:'song1',
+path:'music/Vaa-Vaa-En-Devathaye.mp3',
+artist:'artist 1',
+cover:'images/song1.jpg'
+},
+
+{
+    name:'song2',
+    path:'music/Sevvaanam-Selai-Katti.mp3',
+    artist:'artist 2',
+    cover:'images/song2.jpg'
+    },
+
+    {
+        name:'song3',
+        path:'music/Thoda-Thoda-Malardhadhenna-MassTamilan.dev.mp3',
+        artist:'artist 3',
+        cover:'images/song3.jpg'
+        },
+
+        {
+            name:'song4',
+            path:'music/Un-Sirippinil.mp3',
+            artist:'artist 4',
+            cover:'images/song4.jpg'
+            },
+
+            {
+                name:'song5',
+                path:'music/Kaattu-Sirukki.mp3',
+                artist:'artist 5',
+                cover:'images/song5.jpg'
+                },
+            ]
